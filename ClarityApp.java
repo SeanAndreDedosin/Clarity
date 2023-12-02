9b44ba65d7e0104d453e9d34ce2708bfdd655d9d
bruhmoment3222 <116079110+SeanAndreDedosin@users.noreply.github.com>
2023-12-02 01:13:27 +0000

    New project to push to Github

---
+++ ClarityApp.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package clarityapp;
+
+/**
+ *
+ * @author Andre
+ */
+public class ClarityApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        ClarityGUI myGUI = new ClarityGUI();
+        myGUI.setVisible(true);
+    }
+    
+}

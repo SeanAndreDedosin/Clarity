c6abbb2dc94a170bc385c2ed77cbabc387fdeb96
bruhmoment3222 <116079110+SeanAndreDedosin@users.noreply.github.com>
2023-12-02 20:13:23 +0000

    More files

--- HealthForm.java
+++ HealthForm.java
@@ -4,13 +4,40 @@
  */
 package clarityapp;
 
-/*dhfeja*/
+/*Andre Github*/
+/*Andre Author*/
 
 public abstract class HealthForm {
-    private int question;
+    //declare data members
+    private int mark;
     private String grade;
+    private String fQuestions[];
 
-
+    //constructor
+    public HealthForm(int question, String grade) {
+        mark = 0;
+        grade = " ";
+    }
     
+    //compute
+    public void compute(){
+        if(mark<=0 && mark<=8){
+            grade = "You are all Good!";
+        }
+        else if(mark<=9 && mark<=18){
+            grade = "You have a few underlying issues that need some improvement";
+        }
+        else if(mark<=19 && mark<=24){
+            grade = "It is best to seek Professional Help. Do not be afraid to ask, we are there for you";
+        }
+        else{
+            grade = "Please input Something";
+        }
+    }
+    
+    //getters
+    public String getGrade() {
+        return grade;
+    }
     
 }

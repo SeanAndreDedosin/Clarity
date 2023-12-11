fde8fffccae0727e744526e06dc5b656c69b27bc
bruhmoment3222 <116079110+SeanAndreDedosin@users.noreply.github.com>
2023-12-11 21:39:54 +0000

    Add files via upload

---
+++ HealthForm/HealthFormApp/src/healthformapp/HealthForm.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package healthformapp;
+
+/*Andre Github Main*/
+/*Andre Author*/
+
+public class HealthForm {
+    //declare data members
+    private int mark;
+    private String grade;
+    
+    public HealthForm() {
+        mark = 0;
+        grade = " ";
+    }
+
+    public int getMark() {
+        return mark;
+    }
+
+    public void setMark(int mark) {
+        this.mark = mark;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+
+    public void setGrade(String grade) {
+        this.grade = grade;
+    }
+
+    public int getDetails(){
+        return mark;
+    }
+}
---
+++ HealthForm/HealthFormApp/src/healthformapp/HealthFormApp.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package healthformapp;
+
+/*Andre Github Main*/
+/*Andre Author*/
+
+public class HealthFormApp {
+    
+    public static void main(String[] args) {
+        FormJFrame myJFrame = new FormJFrame();
+        myJFrame.setVisible(true);
+    }
+    
+}
